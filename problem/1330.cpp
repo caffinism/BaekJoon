20b55b6d37424e17834fa7a6910d1581314e726c
dorcus3 <dorcus3@o365.sogang.ac.kr>
2021-07-04 18:15:20 +0900

    1330

--- problem/1330.cpp
+++ problem/1330.cpp
@@ -2,10 +2,10 @@
 
 int main() {
 	int a, b;
-	std::cin >> a >> b;
-	if (a > b) std::cout << '>';
-	else if (a < b) std::cout << '<';
-	else std::cout << "==";
+
+	while (std::cin>>a>>b) {
+		std::cout << a + b << '\n';
+	}
 
 	return 0;
 }
