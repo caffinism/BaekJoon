a59b13274099fbd8322b24a4d82239f34f67bd90
park young jae <cjswoqkqhx@gmail.com>
2021-07-16 22:26:31 +0900

    0만들기

--- problem/test1.cpp
+++ problem/test1.cpp
@@ -1,4 +1,27 @@
 #include <iostream>
-void main() {
-	std::cout << 1;
+using namespace std;
+
+int t, n; [10] ;
+
+int dfs(int v, int val) {
+	if (v == n) {
+		if val==0
+		return val;
+	}
+
+	+
+	-
+	' '
+	
+}
+
+int main() {
+	cin >> t;
+	while (t--) {
+		cin >> n;
+		dfs(0, 0);
+	}
+
+
+	return 0;
 }
